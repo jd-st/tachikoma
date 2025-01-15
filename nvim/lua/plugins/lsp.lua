8e5b6ec2439f049635338e31b827ef5d18647617
Jms Dnns <jdennis@gmail.com>
2025-01-14 19:41:52 -0500

    Updated lsp settings to support ocaml

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,19 @@ return {
       -- This turns off hints by default, but they're still available
       -- by hitting SPC u h
       inlay_hints = { enabled = false },
+      servers = {
+        ocamllsp = {
+          cmd = { "ocamllsp", "--fallback-read-dot-merlin" },
+          filetypes = { "ocaml", "ocaml.interface" },
+          root_dir = require("lspconfig.util").root_pattern(".merlin", ".git"),
+          settings = {
+            ocaml = {},
+          },
+          env = {
+            PATH = vim.fn.expand("$HOME") .. "/.opam/sandbox/bin:" .. vim.env.PATH,
+          },
+        },
+      },
     },
   },
 }
