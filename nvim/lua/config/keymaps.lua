3b4861c532a65e32d0117ca99966cb609d45e6ea
Jms Dnns <jdennis@gmail.com>
2025-03-02 01:05:18 -0500

    Added comment toggle keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<c-/>", "gcc", { remap = true, desc = "Toggle comment for line" })
+vim.keymap.set("v", "<c-/>", "gc", { remap = true, desc = "Toggle comment for selection" })
