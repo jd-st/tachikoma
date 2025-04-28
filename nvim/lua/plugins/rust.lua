1f0ab762181c0d40232f2d7503ad4e83b5044793
Jms Dnns <jdennis@gmail.com>
2025-04-28 16:59:24 -0400

    lazyvim moved to blink so this config is handled elsewhere now

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -4,19 +4,4 @@ return {
     version = "^5",
     lazy = false,
   },
-  {
-    "Saecki/crates.nvim",
-    event = { "BufRead Cargo.toml" },
-    opts = {
-      completion = {
-        cmp = { enabled = true },
-      },
-    },
-    lsp = {
-      enabled = true,
-      actions = true,
-      completion = true,
-      hover = true,
-    },
-  },
 }
