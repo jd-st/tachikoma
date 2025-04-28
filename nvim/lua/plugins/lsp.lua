5b7251bab4c756ca41614b5d7d8cc49511f4211b
Jms Dnns <jdennis@gmail.com>
2025-04-28 17:00:42 -0400

    newer format for ruff lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,34 +25,22 @@ return {
               analysis = {
                 typeCheckingMode = "off", -- "basic" or "strict"
               },
+              pyright = {
+                --disableOrganizeImports = true,
+              },
             },
           },
         },
         ruff = {
-          -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
-          -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
-          commands = {
-            RuffAutofix = {
-              function()
-                vim.lsp.buf.execute_command({
-                  command = "ruff.applyAutofix",
-                  arguments = {
-                    { uri = vim.uri_from_bufnr(0) },
-                  },
-                })
-              end,
-              description = "Ruff: Fix all auto-fixable problems",
-            },
-            RuffOrganizeImports = {
-              function()
-                vim.lsp.buf.execute_command({
-                  command = "ruff.applyOrganizeImports",
-                  arguments = {
-                    { uri = vim.uri_from_bufnr(0) },
-                  },
-                })
-              end,
-              description = "Ruff: Format imports",
+          cmd_env = { RUFF_TRACE = "messages" },
+          settings = {
+            logLevel = "debug",
+          },
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
             },
           },
         },
