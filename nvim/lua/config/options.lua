b89c1d62056cf57aa2c1e15439eb7d3953ee6ecb
Jms Dnns <jdennis@gmail.com>
2025-11-17 22:20:39 -0500

    Removed ocaml from mason

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,15 +10,3 @@ vim.opt.guicursor = "n-v-i-c:block-Cursor"
 -- Python
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
-
--- Ocaml setup
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "ocaml", "ocaml.interface", "ocaml.source", "ocaml.menhir", "ocaml.ocamllex", "reason" },
-  callback = function()
-    vim.opt.tabstop = 2
-    --vim.cmd("set tabstop=2")
-  end,
-})
-vim.opt.rtp:prepend("/Users/jmsdnns/.opam/sandbox/share/ocp-indent/vim")
-local opamshare = vim.fn.system("opam config var share"):gsub("\n$", "")
-vim.opt.rtp:append(opamshare .. "/merlin/vim")
